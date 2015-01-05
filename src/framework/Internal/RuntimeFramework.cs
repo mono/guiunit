d1b69a8357ca2a3aa3da8ccda13b099ce0378616
Marius Ungureanu <teromario@yahoo.com>
2015-01-05 11:51:18 +0200

    Fix leaking this key.

--- src/framework/Internal/RuntimeFramework.cs
+++ src/framework/Internal/RuntimeFramework.cs
@@ -198,6 +198,7 @@ namespace NUnit.Framework.Internal
                                     minor = 0;
                                 }
                             }
+                            key.Close();
                         }
                     }
 
